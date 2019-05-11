57bc4e3a839ec52f260d8fd86f4c9ba85d9496e2
Karsten Silz <karsten.silz@outlook.com>
2019-05-11 08:14:02 +0200

    Fixed mapper warnings.

--- src/main/java/com/betterprojectsfaster/jhipster/test/jhipster6java11/service/mapper/ProductMapper.java
+++ src/main/java/com/betterprojectsfaster/jhipster/test/jhipster6java11/service/mapper/ProductMapper.java
@@ -1,22 +1,25 @@
 package com.betterprojectsfaster.jhipster.test.jhipster6java11.service.mapper;
 
-import com.betterprojectsfaster.jhipster.test.jhipster6java11.domain.*;
+import com.betterprojectsfaster.jhipster.test.jhipster6java11.domain.Product;
 import com.betterprojectsfaster.jhipster.test.jhipster6java11.service.dto.ProductDTO;
-
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity {@link Product} and its DTO {@link ProductDTO}.
  */
 @Mapper(componentModel = "spring", uses = {})
-public interface ProductMapper extends EntityMapper<ProductDTO, Product> {
-
+public interface ProductMapper extends EntityMapper<ProductDTO, Product>
+{
 
     @Mapping(target = "orders", ignore = true)
+    @Mapping(target = "removeOrder", ignore = true)
     Product toEntity(ProductDTO productDTO);
 
-    default Product fromId(Long id) {
-        if (id == null) {
+    default Product fromId(Long id)
+    {
+        if (id == null)
+        {
             return null;
         }
         Product product = new Product();
--- src/main/java/com/betterprojectsfaster/jhipster/test/jhipster6java11/service/mapper/ShoppingOrderMapper.java
+++ src/main/java/com/betterprojectsfaster/jhipster/test/jhipster6java11/service/mapper/ShoppingOrderMapper.java
@@ -1,15 +1,16 @@
 package com.betterprojectsfaster.jhipster.test.jhipster6java11.service.mapper;
 
-import com.betterprojectsfaster.jhipster.test.jhipster6java11.domain.*;
+import com.betterprojectsfaster.jhipster.test.jhipster6java11.domain.ShoppingOrder;
 import com.betterprojectsfaster.jhipster.test.jhipster6java11.service.dto.ShoppingOrderDTO;
-
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * Mapper for the entity {@link ShoppingOrder} and its DTO {@link ShoppingOrderDTO}.
  */
-@Mapper(componentModel = "spring", uses = {UserMapper.class})
-public interface ShoppingOrderMapper extends EntityMapper<ShoppingOrderDTO, ShoppingOrder> {
+@Mapper(componentModel = "spring", uses = { UserMapper.class })
+public interface ShoppingOrderMapper extends EntityMapper<ShoppingOrderDTO, ShoppingOrder>
+{
 
     @Mapping(source = "buyer.id", target = "buyerId")
     @Mapping(source = "buyer.login", target = "buyerLogin")
@@ -18,10 +19,13 @@ public interface ShoppingOrderMapper extends EntityMapper<ShoppingOrderDTO, Shop
     @Mapping(target = "orders", ignore = true)
     @Mapping(source = "buyerId", target = "buyer")
     @Mapping(target = "shipment", ignore = true)
+    @Mapping(target = "removeOrders", ignore = true)
     ShoppingOrder toEntity(ShoppingOrderDTO shoppingOrderDTO);
 
-    default ShoppingOrder fromId(Long id) {
-        if (id == null) {
+    default ShoppingOrder fromId(Long id)
+    {
+        if (id == null)
+        {
             return null;
         }
         ShoppingOrder shoppingOrder = new ShoppingOrder();
